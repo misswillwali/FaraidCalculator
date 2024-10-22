9368d11e91970b2b5872a1e5b2c0e318f96ed05b
misswillwali <151745275+misswillwali@users.noreply.github.com>
2024-10-22 23:56:23 +0800

    Create calculator.tsx

---
+++ pages/calculator.tsx
@@ -0,0 +1,11 @@
+// File: pages/calculator.tsx
+import Layout from '@/components/Layout'
+import FaraidCalculator from '@/components/FaraidCalculator'
+
+export default function CalculatorPage() {
+  return (
+    <Layout title="Calculator | Willwali Faraid Calculator">
+      <FaraidCalculator />
+    </Layout>
+  )
+}
