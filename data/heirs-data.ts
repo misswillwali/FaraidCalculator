317d65d4d83d7fc67da5c92fb9f891a11764fb64
misswillwali <151745275+misswillwali@users.noreply.github.com>
2024-10-22 23:47:38 +0800

    Create heirs-data.ts

---
+++ data/heirs-data.ts
@@ -0,0 +1,29 @@
+// File: data/heirs-data.ts
+export const maleHeirs = [
+  { value: "son", label: "Son" },
+  { value: "husband", label: "Husband" },
+  { value: "grandson", label: "Grandson" },
+  { value: "father", label: "Father" },
+  { value: "paternalGrandFather", label: "Paternal Grandfather" },
+  { value: "brother", label: "Brother" },
+  { value: "halfBrotherSameFather", label: "Half Brother (by same father)" },
+  { value: "halfBrotherSameMother", label: "Half Brother (by same mother)" },
+  { value: "nephewBrother", label: "Nephew (brother's son)" },
+  { value: "nephewHalfBrother", label: "Nephew (half-brother's son)" },
+  { value: "paternalUncle", label: "Paternal Uncle" },
+  { value: "fathersHalfBrother", label: "Father's Half Brother (by same father)" },
+  { value: "sonOfPaternalUncle", label: "Son of Paternal Uncle" },
+  { value: "sonOfFathersHalfBrother", label: "Son of Father's Half Brother (by same father)" },
+]
+
+export const femaleHeirs = [
+  { value: "daughter", label: "Daughter" },
+  { value: "wife", label: "Wife" },
+  { value: "sister", label: "Sister" },
+  { value: "granddaughter", label: "Grand-daughter (son's daughter)" },
+  { value: "mother", label: "Mother" },
+  { value: "maternalGrandmother", label: "Maternal grandmother" },
+  { value: "paternalGrandMother", label: "Paternal grandmother" },
+  { value: "halfSisterSameFather", label: "Half Sister (by same father)" },
+  { value: "halfSisterSameMother", label: "Half Sister (by same mother)" },
+]
