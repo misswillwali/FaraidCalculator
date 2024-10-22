84c2d010dd07726bb7bf6360d09b26643bef6ecb
misswillwali <151745275+misswillwali@users.noreply.github.com>
2024-10-22 23:55:56 +0800

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+// File: pages/index.tsx
+import Layout from '@/components/Layout'
+import LandingPage from './landing'
+
+export default function Home() {
+  return (
+    <Layout>
+      <LandingPage />
+    </Layout>
+  )
+}
